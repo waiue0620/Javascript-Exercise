9091b716fd4bf0ebd6c0198da438b0778b2197ad
waiue0620 <39615588+waiue0620@users.noreply.github.com>
2022-10-11 18:50:47 +0800

    Create test.js
    add test js

---
+++ test.js
@@ -0,0 +1,13 @@
+!function(t, e, n) {
+  console.log(t, e, n, 't, e, n');
+  const floatingBtn = document.getElementById('mantaGO-floating-button');
+  floatingBtn.style.position = 'fixed'
+  floatingBtn.style.width = '60px';
+  floatingBtn.style.height = '60px';
+  floatingBtn.style.overflow = 'hidden';
+  floatingBtn.style.right = '20px';
+  floatingBtn.style.bottom = '21px';
+  floatingBtn.style.background = 'transparent';
+  floatingBtn.style.border = 'none';
+  floatingBtn.style.zIndex = '9999';
+}
